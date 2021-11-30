26b4fef3a0104cb79d180603cbbc75809c93393d
metin.aralioglu <metin.aralioglu@ajanspress.com.tr>
2021-11-30 17:02:33 +0300

    aa

---
+++ BulkyBook.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) :base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product obj)
+        {
+
+            //Update updates all properties even when they are not changed
+            //_db.Products.Update(obj);
+
+            //Correct Way to update;
+            //Get the object from db.SO ef core can track the object.
+            //Choose what props to update this way
+            var objFromDb = _db.Products.FirstOrDefault(u=>u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+                objFromDb.CoverTypeId = obj.CoverTypeId;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl=obj.ImageUrl;
+                }
+            }
+        }
+    }
+}
---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,132 @@
+﻿
+using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BulkyBookWeb.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+        //Upsert => Insert(Create) + Update short
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new()
+            {
+                Product = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem{
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            //Product product = new();
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
+            //   u => new SelectListItem
+            //   {
+            //       Text = u.Name,
+            //       Value = u.Id.ToString(),
+            //   }
+            //);
+
+            //IEnumerable<SelectListItem> CoverTypeList = _unitOfWork.CoverType.GetAll().Select(
+            //u => new SelectListItem
+            //{
+            //    Text = u.Name,
+            //    Value = u.Id.ToString(),
+            //}
+            //    );
+
+            if (id == null || id == 0)
+            {
+                //Create Product. Id yoksa create istiyoruz demek.
+                //ViewBag dinamik data. Modelde olmayan verileri viewa yollamak için kullanılır.
+                //ViewBag.CategoryList = CategoryList;
+                //ViewData dinamik değil cast yapmak lazım
+                //ViewData["CoverTypeList"] = CoverTypeList;
+                //return View(product);
+                //Tempdata errorlar için kullanılır.Nullcheck gerekir runtime hatası olmaması için
+
+                return View(productVM);
+            }
+            else
+            {
+                //Update Product. Id varsa update demek.
+            }
+
+            return View(productVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                //_unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product edited successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+
+        }
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var coverTypeFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+
+            if (coverTypeFromDbFirst == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypeFromDbFirst);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.Product.GetFirstOrDefault(c => c.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted successfully!";
+            return RedirectToAction("Index");
+        }
+
+    }
+}
